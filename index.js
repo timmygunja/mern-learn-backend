f35cc794579b3707b94fde4afb27fc2da061e3d3
timmygunja <t.urusov13@mail.ru>
2023-02-12 17:03:53 +0300

    deploy fixes

--- index.js
+++ index.js
@@ -48,7 +48,8 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500);
+  // res.status(error.code || 500);
+  res.status(error.status || 500);
   res.json({ message: error.message || "An unknown error occurred!" });
 });
 
