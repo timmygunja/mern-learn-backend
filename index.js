6b86750d3ed2065a6d916a5e46c4fe519d2ff005
timmygunja <t.urusov13@mail.ru>
2023-02-16 22:22:10 +0300

    deploy fixes

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/uploads/images", express.static(path.join("uploads", "images")));
-app.use('/uploads/images', express.static('uploads/images'));
+// app.use("/uploads/images", express.static(path.join("uploads", "images")));
+// app.use('/uploads/images', express.static('uploads/images'));
+app.use("/uploads/images", express.static(path.join(__dirname, "./uploads/images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
